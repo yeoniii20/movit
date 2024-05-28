d3e104b87061f5e4bed5bc72fe46a764dc8f0f7e
yeoniii20 <commetoi_yeoni@naver.com>
2024-05-28 23:13:37 +0900

    feat: add header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
+<script setup></script>
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <!-- <nav>
+    <Routerlink to="/">Home</Routerlink>
+    <span> | </span>
+    <RouterLink to="/about">About</RouterLink>
+  </nav> -->
+  <main>
+    <HeaderCom />
+    <RouterView></RouterView>
+    <FooterCom />
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HeaderCom from "./components/HeaderCom.vue";
+import FooterCom from "./components/FooterCom.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    HeaderCom,
+    FooterCom,
   },
 };
 </script>
-
 <style>
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+header {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background-color: #333;
+  color: white;
+  padding: 1em 0;
+  z-index: 1000;
+}
+
+main {
+  flex: 1;
+  margin-top: 60px; /* 헤더의 높이만큼 마진 */
+}
+
+footer {
+  background-color: #333;
+  color: white;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 1em 0;
 }
 </style>
