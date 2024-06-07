63636b3284fb6d9bb50085db4e7ea2e6a7a7ce79
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-06-07 13:15:12 +0900

    feat: create CanvasView

---
+++ src/views/CanvasView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="btn-container">
+      <div class="btn" @click="downloadCanvas">Download</div>
+    </div>
+    <CanvasCom ref="canvasCom" />
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import CanvasCom from "@/components/CanvasCom.vue";
+
+export default {
+  name: "CanvasView",
+  components: {
+    CanvasCom,
+  },
+  setup() {
+    const canvasCom = ref(null);
+
+    const downloadCanvas = () => {
+      canvasCom.value.downloadCanvas();
+    };
+
+    return {
+      canvasCom,
+      downloadCanvas,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.btn-container {
+  display: flex;
+}
+
+.btn {
+  border-radius: 1rem;
+  background-color: burlywood;
+  padding: 0.25rem;
+  cursor: pointer;
+}
+</style>
