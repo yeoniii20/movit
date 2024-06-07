1b7b6600b296b86a90f46467c742b1b5a4c75722
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-06-07 13:39:32 +0900

    feat: change color and add clear btn

--- src/views/CanvasView.vue
+++ src/views/CanvasView.vue
@@ -2,8 +2,18 @@
   <div>
     <div class="btn-container">
       <div class="btn" @click="downloadCanvas">Download</div>
+      <div class="btn" @click="clearCanvas">Clear</div>
     </div>
-    <CanvasCom ref="canvasCom" />
+    <div class="crayon-container">
+      <div
+        v-for="color in colors"
+        :key="color"
+        class="crayon"
+        :style="{ backgroundColor: color }"
+        @click="selectColor(color)"
+      ></div>
+    </div>
+    <CanvasCom ref="canvasCom" :selectedColor="selectedColor" />
   </div>
 </template>
 
@@ -18,14 +28,37 @@ export default {
   },
   setup() {
     const canvasCom = ref(null);
+    const selectedColor = ref("rgba(255, 0, 0)");
+
+    const colors = [
+      "red",
+      "orange",
+      "yellow",
+      "green",
+      "blue",
+      "indigo",
+      "violet",
+    ];
+
+    const selectColor = (color) => {
+      selectedColor.value = color;
+    };
 
     const downloadCanvas = () => {
       canvasCom.value.downloadCanvas();
     };
 
+    const clearCanvas = () => {
+      canvasCom.value.clearCanvas();
+    };
+
     return {
       canvasCom,
       downloadCanvas,
+      clearCanvas,
+      colors,
+      selectColor,
+      selectedColor,
     };
   },
 };
@@ -40,6 +73,21 @@ export default {
   border-radius: 1rem;
   background-color: burlywood;
   padding: 0.25rem;
+  cursor: pointer;
+  margin-right: 1rem;
+}
+
+.crayon-container {
+  display: flex;
+  justify-content: center;
+  margin: 1rem 0;
+}
+
+.crayon {
+  width: 30px;
+  height: 100px;
+  margin: 0 5px;
+  border-radius: 5px;
   cursor: pointer;
 }
 </style>
