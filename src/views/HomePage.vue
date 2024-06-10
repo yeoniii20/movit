9d258aeac4ddf3cffbf3cbab7279f65234a44689
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-06-10 10:48:24 +0900

    feat: add more btn

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Welcome to MovieSearch</h1>
+    <h1>Welcome to Movit</h1>
     <p>Search for your favorite movies and more.</p>
     <div class="card-container">
       <CardCom
@@ -11,6 +11,9 @@
         :description="movie.description"
         :id="movie.id"
       />
+    </div>
+    <div class="load-more-container">
+      <button class="load-more-btn" @click="loadMoreMovies">더보기</button>
     </div>
   </div>
 </template>
@@ -27,24 +30,30 @@ export default {
   data() {
     return {
       movies: [],
+      page: 1,
     };
   },
   methods: {
     async fetchMovies() {
       const apiKey = process.env.VUE_APP_TMDB_API_KEY;
-      const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=ko-KR&region=KR&page=1`; // Add region parameter to fetch only Korean movies
+      const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=ko-KR&region=KR&page=${this.page}`; // Add region parameter to fetch only Korean movies
       try {
         const response = await axios.get(url);
-        this.movies = response.data.results.map((movie) => ({
+        const newMovies = response.data.results.map((movie) => ({
           id: movie.id,
           image: `https://image.tmdb.org/t/p/w300${movie.poster_path}`,
           title: movie.title,
           description: movie.overview,
         }));
+        this.movies = [...this.movies, ...newMovies];
         console.log("Fetched movies:", this.movies);
       } catch (error) {
         console.error("Error fetching movies:", error);
       }
+    },
+    loadMoreMovies() {
+      this.page += 1;
+      this.fetchMovies();
     },
   },
   created() {
@@ -69,5 +78,20 @@ p {
   justify-content: center;
   gap: 1em;
   margin-top: 2em;
+  margin-bottom: 2em;
+}
+
+.load-more-container {
+  text-align: center;
+  margin-top: 2em;
+  margin-bottom: 4em;
+}
+
+.load-more-btn {
+  border-radius: 0.25rem;
+  background-color: rgb(195, 195, 195);
+  padding: 0.25rem 0.75rem;
+  cursor: pointer;
+  border-color: transparent;
 }
 </style>
