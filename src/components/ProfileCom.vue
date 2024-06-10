e325bd0f564bf8beb53304758371a3eacb524a7d
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-06-10 14:46:17 +0900

    feat: create Profile component

---
+++ src/components/ProfileCom.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="profile-selector">
+    <div class="icon-list">
+      <span
+        v-for="icon in icons"
+        :key="icon"
+        class="icon"
+        @click="selectIcon(icon)"
+      >
+        {{ icon }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProfileCom",
+  data() {
+    return {
+      icons: [
+        "👤",
+        "👥",
+        "🧑",
+        "🧑🏻",
+        "🧑🏼",
+        "🧑🏽",
+        "🧑🏾",
+        "🧑🏿",
+        "👨",
+        "👨🏻",
+        "👨🏼",
+        "👨🏽",
+        "👨🏾",
+        "👨🏿",
+        "👩",
+        "👩🏻",
+        "👩🏼",
+        "👩🏽",
+        "👩🏾",
+        "👩🏿",
+      ],
+    };
+  },
+  methods: {
+    selectIcon(icon) {
+      this.$emit("updateProfile", icon);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.icon-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 10px;
+}
+
+.icon {
+  cursor: pointer;
+  font-size: 2em;
+  transition: transform 0.2s;
+}
+
+.icon:hover {
+  transform: scale(1.2);
+}
+</style>
